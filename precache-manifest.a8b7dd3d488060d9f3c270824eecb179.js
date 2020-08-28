9ddba3d58aac6024894bead6a9c62f3ba2c3e143
sandeepkey62 <sandeepkanoujiya1@gmail.com>
2020-08-29 01:23:38 +0530

    Updates

---
+++ precache-manifest.a8b7dd3d488060d9f3c270824eecb179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8b6b28af761d33ef696798325101ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241d79300d5ee61305a0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c922376c05bb3b1d2f9d",
+    "url": "/static/js/2.e9fc4b10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9fc4b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241d79300d5ee61305a0",
+    "url": "/static/js/main.0ab8b4a3.chunk.js"
+  },
+  {
+    "revision": "4e298d7ae7623fec8b7e",
+    "url": "/static/js/runtime-main.b85d870a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
